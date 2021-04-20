e22dd363ef3810572ff7c5aadd5d471d8e2a7432
tomaSalas <tomassubero97@gmail.com>
2021-04-20 14:38:14 -0500

    added form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "React";
+import React from "react";
+import "./static/index.css";
+
+//components
+import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
     <div className="container">
-      
+      <SearchForm  />
     </div>
   );
 }
